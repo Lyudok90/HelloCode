e18c6a9d34d3e99567047e7a558e5340a11a4024
Lydok90 <lyudaharutyunyan@gmail.com>
2023-04-12 23:56:49 +0400

    ifelse

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter user name:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ura, this is Masha!");
+}
+else
+{
+    Console.Write("hello, ");
+    Console.WriteLine(username);
+
+}
